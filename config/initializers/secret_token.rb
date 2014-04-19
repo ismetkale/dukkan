209606154d72aaadb804920b50c297bd26eb3f23
ismetkale <ismetkale@gmail.com>
2014-04-19 04:05:06 +0300

    First Commit-Dukkan to GitHub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dukkan::Application.config.secret_key_base = '8dd1242162cba7264ddb29dd02940422d2687da4f812d131988864c1eded9407c4f193b2c6d6d062d60031e0b7058e7f40b1547ef80e6ecd32d8e3d2b9a0b829'
