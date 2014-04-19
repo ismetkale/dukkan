2d749b85170f2669b9c5158345797761ccc4a8c5
ismetkale <ismetkale@gmail.com>
2014-04-20 01:00:40 +0300

    Temporarily remove images from homepage

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,13 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "no.jpg"
+  has_attached_file :image, 
+  					:styles => { :medium => "200x", :thumb => "100x100>" }, 
+  					:default_url => "no.jpg",
+					:storage => :dropbox,
+    				:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+    				
+
   validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
+
+
+  
 end
