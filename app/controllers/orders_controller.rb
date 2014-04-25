bf9136f191f275fa91a36f31c2fbeba1d0a2f69d
ismetkale <ismetkale@gmail.com>
2014-04-25 06:35:47 +0300

    Transfer funds to seller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -46,6 +46,13 @@ class OrdersController < ApplicationController
     end
 
 
+      transfer = Stripe::Transfer.create(
+      :amount => (@listing.price * 95).floor,
+      :currency => "usd",
+      :recipient => @seller.recipient
+      )
+
+
 
     respond_to do |format|
       if @order.save
